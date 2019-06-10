c156e2a80b018196660a9b431540db275f4b12d1
ErikJP <erik.pronk@colorado.edu>
2019-06-10 22:32:25 +0200

    Adding more to prim file

--- ErikWorkingFolder/load_results_and_PRIM.py
+++ ErikWorkingFolder/load_results_and_PRIM.py
@@ -12,6 +12,8 @@ from ema_workbench import (Model, CategoricalParameter, ScalarOutcome,
 
 from ema_workbench.util.utilities import load_results
 
+from ema_workbench.analysis import prim
+
 def load_tar(dike_names):
 	results = {}
 	for name in dike_names:
@@ -28,8 +30,33 @@ def get_exp_and_outcomes(results):
 
 	return experiments, outcomes
 
+def do_PRIM(experiments, outcomes, outcome_name='Expected Annual Damage'):
+	uncert = {}
+	levers = {}
+	data = {}
+	prim_alg_uncert = {}
+	prim_alg_levers = {}
+	box_uncert = {}
+	box_levers = {}
+
+	for key in experiments.keys():
+		uncert[key] = experiments[key].drop(experiments[key].columns[16:-2], axis=1)
+		levers[key] = experiments[key].iloc[:,16:-1]
+		data[key] = outcomes['A.1'][outcome_name+' '+key.replace('.','')]
+
+		prim_alg_uncert[key] = prim.Prim(uncert[key], data[key], threshold=0.6)
+		prim_alg_levers[key] = prim.Prim(levers[key], data[key], threshold=0.6)
+
+		box_uncert[key] = prim_alg_uncert.find_box()
+		box_levers[key] = prim_alg_levers.find_box()
+
+	return
+
 if __name__ == '__main__':
 	dike_names = ['A.1','A.2','A.3','A.4','A.5']
 	results = load_tar(dike_names)
 	experiments, outcomes = get_exp_and_outcomes(results)
+	print(outcomes['A.1'])
+	# outcome_name is 'Expected Annual Damage' (default), 'Expected Number of Deaths in', or 'Investment Costs'
+	do_PRIM(experiments, outcomes)
 	
