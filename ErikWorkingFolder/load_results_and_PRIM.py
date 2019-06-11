62f01d917279e8af07e9c8c4b03260c68abecd67
ErikJP <erik.pronk@colorado.edu>
2019-06-11 15:21:44 +0200

    working on PRIM

--- ErikWorkingFolder/load_results_and_PRIM.py
+++ ErikWorkingFolder/load_results_and_PRIM.py
@@ -24,9 +24,12 @@ def load_tar(dike_names):
 def get_exp_and_outcomes(results):
 	experiments = {}
 	outcomes = {}
+	print(results['A.1'][1])
 	for key in results.keys():
 		experiments[key] = results[key][0]
 		outcomes[key] = results[key][1]
+
+		# print(outcomes['A.1'])
 
 	return experiments, outcomes
 
@@ -34,6 +37,7 @@ def do_PRIM(experiments, outcomes, outcome_name='Expected Annual Damage'):
 	uncert = {}
 	levers = {}
 	data = {}
+	y = {}
 	prim_alg_uncert = {}
 	prim_alg_levers = {}
 	box_uncert = {}
@@ -42,10 +46,14 @@ def do_PRIM(experiments, outcomes, outcome_name='Expected Annual Damage'):
 	for key in experiments.keys():
 		uncert[key] = experiments[key].drop(experiments[key].columns[16:-2], axis=1)
 		levers[key] = experiments[key].iloc[:,16:-1]
-		data[key] = outcomes['A.1'][outcome_name+' '+key.replace('.','')]
+		data[key] = outcomes[key][outcome_name+' '+key.replace('.','')]
 
-		prim_alg_uncert[key] = prim.Prim(uncert[key], data[key], threshold=0.6)
-		prim_alg_levers[key] = prim.Prim(levers[key], data[key], threshold=0.6)
+		# print(outcomes[key])
+
+		y[key] = data[key] < np.percentile(data[key], 10)
+
+		prim_alg_uncert[key] = prim.Prim(uncert[key], y[key], threshold=0.6)
+		prim_alg_levers[key] = prim.Prim(levers[key], y[key], threshold=0.6)
 
 		box_uncert[key] = prim_alg_uncert.find_box()
 		box_levers[key] = prim_alg_levers.find_box()
@@ -56,7 +64,6 @@ if __name__ == '__main__':
 	dike_names = ['A.1','A.2','A.3','A.4','A.5']
 	results = load_tar(dike_names)
 	experiments, outcomes = get_exp_and_outcomes(results)
-	print(outcomes['A.1'])
 	# outcome_name is 'Expected Annual Damage' (default), 'Expected Number of Deaths in', or 'Investment Costs'
 	do_PRIM(experiments, outcomes)
 	
